09fd367cc55a52a9817deb995297fab3d30746f5
dinbelau-alt <dinbelau@gmail.com>
2026-04-29 18:38:50 +0300

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "MADE WITH 𝑔𝒽𝑜𝓈𝓉 ",
+        name: "MADE WITH 𝑔𝒽𝑜𝓈𝓉  ",
         // Activity type number (0 = Playing).
         type: 2, 
       },
